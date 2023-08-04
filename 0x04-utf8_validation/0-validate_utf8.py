995fc0559ae530a654eac69864321a220465c60f
mazi-kunle <olakunlemichael644@gmail.com>
2023-08-04 16:59:02 +0100

    task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -14,18 +14,26 @@ def validUTF8(data):
     for i in data:
         i = int(i)
         if remainder == 0:
-            if (i >> 7 == 0):
+            if (bin(i >> 7) == '0b0'):
                 remainder = 0
-            elif (i >> 5 == 110):
+            elif (bin(i >> 5) == '0b110'):
                 remainder = 1
             elif (i >> 4 == 1110):
                 remainder = 2
-            elif (i >> 3 == 11110):
+            elif (bin(i >> 3) == '0b11110'):
                 remainder = 3
             else:
                 return False
         else:
-            if (i >> 6 == 10):
+            if (bin(i >> 6) == '0b10'):
                 remainder = remainder - 1
 
     return (remainder == 0)
+
+# data = [65]
+# print(validUTF8(data)) # True
+
+# data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
+# print(validUTF8(data)) # True
+# data = [229, 65, 127, 256]
+# print(validUTF8(data)) # False
