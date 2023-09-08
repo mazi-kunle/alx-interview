8800af24998316ad2903e35b747e58e508269893
mazi-kunle <olakunlemichael644@gmail.com>
2023-09-08 23:31:24 +0100

    complete task

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''This is a module'''
+
+
+def island_perimeter(grid):
+    row = len(grid)
+    col = len(grid[0])
+    p = 0
+    connected = 0
+    for i in range(row):
+        for j in range(col):
+            if grid[i][j] == 1:
+                p = p + 4
+                if i > 0 and grid[i-1][j]:
+                    connected = connected + 1
+                if j > 0 and grid[i][j-1]:
+                    connected = connected + 1
+                    print(i, j)
+
+    return p - (connected * 2)
+
+
+# grid = [
+#     [0, 0, 0, 0, 0, 0],
+#     [0, 1, 0, 0, 0, 0],
+#     [0, 1, 0, 0, 0, 0],
+#     [0, 1, 1, 1, 0, 0],
+#     [0, 0, 0, 0, 0, 0]
+# ]
